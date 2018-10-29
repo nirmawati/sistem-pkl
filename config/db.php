a805bb5d06eb4448b11d9ab2ff1556baa702719e
aslampangestu <aslampangestu@gmail.com>
2018-10-29 11:42:34 +0700

    remove db

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dbpklais',
     'username' => 'root',
-    'password' => 'Milanisti03',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
